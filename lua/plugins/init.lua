f70037d3756b97d69ec8c5dc94bccdbf22232056
Nishant Chhillar <nchhillar2004@gmail.com>
2024-11-20 18:32:12 +0530

    Initial commit

---
+++ lua/plugins/init.lua
@@ -0,0 +1,16 @@
+return {
+    { 'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' } },
+    { 'rose-pine/neovim', name = 'rose-pine' },
+    { 'sainnhe/sonokai' },
+    { 'catppuccin/nvim' },
+    { 'mg979/vim-visual-multi', branch = 'master' },
+    { 'tpope/vim-fugitive' },
+    { 'tpope/vim-sensible' },
+    { 'preservim/nerdtree' },
+    { 'vim-airline/vim-airline' },
+    { 'neoclide/coc.nvim', branch = 'release' },
+    { 'echasnovski/mini.icons', version = '*' },
+    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+    { 'prettier/vim-prettier' },
+    { 'akinsho/bufferline.nvim', version = "*", dependencies = { 'nvim-tree/nvim-web-devicons' } }
+}
