9e7f4af4af744c55bf30e2a49479b35a52435585
Nishant Chhillar <nchhillar2004@gmail.com>
2024-11-25 19:03:17 +0530

    stable commit

--- init.lua
+++ init.lua
@@ -1,14 +1,15 @@
 require("config.lazy")
 require("config.keymaps")
 require("config.settings")
+require("config.plugins")
 
 -- setup languages for better syntax highlight
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"typescript", "java", "go", "c", "cpp", "tsx", "rust", "python", "lua", "r", "haskell", "fortran", "markdown", "html", "css"},
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+    ensure_installed = {"typescript", "java", "go", "c", "cpp", "tsx", "rust", "python", "lua", "r", "haskell", "fortran", "markdown", "html", "css"},
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
 }
 
 -- Install Coc LSP using the following command
